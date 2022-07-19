c0f32b7a397ea077b3e8bf33c81899760d7de1cc
Braab6 <be.raab2000@gmail.com>
2022-07-19 17:44:42 +0200

    a

--- DiscordBot.py
+++ DiscordBot.py
@@ -26,7 +26,7 @@ token = "OTk2MTQ1Mjc5OTEwODEzNzc2.GPdTto.f_FCHy1E5alVIakbyFQUO5h7_dVCGOxsWFDNB4"
                 )
             ]
         )
-    ]   #assssqwaasdfe
+    ]   
 )
 async def _releases(ctx:SlashContext, option:str):
     await ctx.send(option)
