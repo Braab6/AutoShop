e57f89b50ec4719f46c20958193c6e2a3b59d1bf
Braab6 <be.raab2000@gmail.com>
2022-07-19 22:07:58 +0200

    19.07

--- automatedShoppingv1_0.py
+++ automatedShoppingv1_0.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.support.select import Select
 from tkinter import *
-
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 
 root = Tk()
@@ -25,25 +25,47 @@ def Zalando ():
 
 
 def Nike ():
-    driver = webdriver.Chrome("C:\\Users\\49152\\Downloads\\chromedriver_win32\\chromedriver.exe")
-    driver.get("https://www.nike.com/de/t/air-jordan-1-low-herrenschuh-PPffMw/553558-130")
 
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(chrome_options=options, executable_path=r'C:\\Users\\49152\\Downloads\\chromedriver_win32\\chromedriver.exe')
+    driver.get("https://www.nike.com/de/t/air-jordan-1-low-herrenschuh-PPffMw/553558-130")
+   
     time.sleep(3.5)
+
+
     print("smth")
 
     cookie = driver.find_element(By.ID, "hf_cookie_text_cookieAccept").click()
-    time.sleep(1)
-
  
-    size = driver.find_element(By.ID, "skuAndSize__24375104").click()
+    size = driver.find_element(By.ID, "skuAndSize__24375214")
+    print("smth")
+    driver.execute_script("arguments[0].scrollIntoView();", size)
+    time.sleep(1)
+    driver.execute_script("arguments[0].click();", size)
+    time.sleep(1)
+    cart = driver.find_element(By.XPATH, "//button[@aria-label='In den Warenkorb']")
+    time.sleep(1)
+    driver.execute_script("arguments[0].click();", cart)
 
-#'ASDFFFFF'
+    #time.sleep(100)
+  
+    
+
 root.title("AutoBot")
 root.geometry("300x300")
+root.resizable(False, False)
+
 Zalandobutton = Button(root, text="Zalando", command=Zalando)
 Zalandobutton.pack()
+
 Nikebutton = Button(root, text="Nike", command=Nike)
 Nikebutton.pack()
+
+closeButton = Button(root, text="Close", command=root.destroy)
+closeButton.place(x=150, y=100)
+
+
 
 root.mainloop()
 
